0b62e050028e7c89a436f836ffdfef51419bc135
Nurlan Mukhambetov <nurlanm@Nurlans-MacBook-Pro-2.local>
2023-11-30 11:46:32 -0600

    pushing initial files

---
+++ src/test/java/CucumberRunnerTest.java
@@ -0,0 +1,19 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"html:target/cucumber.html", "json:target/report.json"},
+        features = "src/test/resources/features",
+        glue = "steps",
+        tags = "@smoke",
+        dryRun = false // when we do only for methods in console , to do steps we do true , to run after we do false
+)
+
+public class CucumberRunnerTest {
+
+
+
+
+}
---
+++ src/test/resources/features/SmokeTest.feature
@@ -0,0 +1,5 @@
+Feature: this feature is for smoke tests
+  @smoke
+  Scenario: Smoke Test
+    Given I print hello world
+    Then everybody is happy
